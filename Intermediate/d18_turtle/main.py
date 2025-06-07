06dcb1c85e11b2a4d7d98249d9dc94d84cd8c149
John <johnzionwong33@gmail.com>
2025-06-07 10:45:48 -0700

    d18_turtle_hirst part done

--- Intermediate/d18_turtle/main.py
+++ Intermediate/d18_turtle/main.py
@@ -37,11 +37,30 @@ screen.colormode(255)
 
 # spirograph
 # tim.pensize(5)
-def draw_spirograph(size_of_gap):
-    for _ in range(int(360 / size_of_gap)):
-        tim.color((random.randint(0,255), random.randint(0,255), random.randint(0,255)))
-        tim.circle(100)
-        tim.setheading(tim.heading() + size_of_gap)
-draw_spirograph(5)
+# def draw_spirograph(size_of_gap):
+#     for _ in range(int(360 / size_of_gap)):
+#         tim.color((random.randint(0,255), random.randint(0,255), random.randint(0,255)))
+#         tim.circle(100)
+#         tim.setheading(tim.heading() + size_of_gap)
+# draw_spirograph(5)
+
+# hirst painting
+color_list = [ (200, 10, 35), (247, 236, 37), (240, 244, 251), (239, 231, 3), (36, 216, 77), (223, 159, 61), (40, 79, 185), (28, 39, 159), (210, 73, 16), (17, 151, 18), (239, 39, 152), (65, 9, 27), (188, 14, 9), (216, 25, 127), (218, 140, 198), (223, 161, 7), (59, 12, 7), (67, 202, 227), (10, 96, 60), (84, 80, 212), (17, 19, 52), (237, 157, 218), (106, 232, 195), (99, 205, 136), (212, 84, 58), (8, 222, 235), (236, 171, 161)]
+tim.penup()
+tim.hideturtle()
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
 
 screen.exitonclick()
