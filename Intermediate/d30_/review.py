2e63c9317dddee0fe6f7a03db8c452a0d070a6f9
John <johnzionwong33@gmail.com>
2025-06-11 12:26:38 -0700

    exception/error handling review

---
+++ Intermediate/d30_/review.py
@@ -0,0 +1,13 @@
+# Examples of Exceptions/Errors
+    # FileNotFound
+    # KeyError
+    # IndexError
+    # TypeError
+
+# Catching Exceptions/Errors
+    # try: # something that might cause an exception
+    # except ErrorName: # do this if there was an exception, can also do "except ErrorName as error_msg:" and use the msg
+    # else: # do this if there were no exceptions
+    # finally: # do this no matter what happens
+
+    # raise: # raise your own exception (ex. "raise TypeError("This is a TypeError I raised")")
