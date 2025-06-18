aadb2f3ebbbb1745c56276c8d1bd83e738c934b6
John <johnzionwong33@gmail.com>
2025-06-18 14:03:39 -0700

    d45 movie scraping done

--- Intermediate+/d45_web_scraping/main/main.py
+++ Intermediate+/d45_web_scraping/main/main.py
@@ -1,8 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
 
 # Write your code below this line 👇
+top_movies_response = requests.get(URL)
+top_movies_response.raise_for_status()
+top_movies_html = top_movies_response.text
 
+soup = BeautifulSoup(top_movies_html, "html.parser")
 
+movie_tags = soup.find_all(name="h2") # extract h2s, then strong below
+movie_names = [tag.get_text(strip=True) for tag in movie_tags if tag.find("strong")] # <strong> is a nested tag
+
+movie_names = movie_names[::-1]    
+print(movie_names)
+
+with open("movies.txt", mode="w") as file:
+    for movie in movie_names:
+        file.write(f"{movie}\n") 
