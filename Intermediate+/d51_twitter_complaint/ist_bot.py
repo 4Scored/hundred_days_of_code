2aeb03aeb5f421655720547ee7a72ec575ed5c91
John <johnzionwong33@gmail.com>
2025-06-22 19:28:19 -0700

    d51_twitter_complaint reformatting and finish

--- Intermediate+/d51_twitter_complaint/ist_bot.py
+++ Intermediate+/d51_twitter_complaint/ist_bot.py
@@ -2,9 +2,13 @@ import os
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 
+PROMISED_DOWN = 150
+PROMISED_UP = 10
 INTERNET_SPEED_URL = "https://www.speedtest.net/"
+TWITTER_LOGIN_URL = "https://twitter.com/login"
 
 class InternetSpeedTwitterBot:
     
@@ -27,4 +31,20 @@ class InternetSpeedTwitterBot:
 
 
     def tweet_at_provider(self):
-        pass
+        self.driver.get(TWITTER_LOGIN_URL)
+        sleep(5)
+        email = self.driver.find_element(By.XPATH, value="//*[@id='layers']/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[4]/label/div/div[2]/div/input")
+        email.send_keys(os.environ.get("TWITTER_EMAIL"))
+        email.send_keys(Keys.ENTER)
+        sleep(5)
+        password = self.driver.find_element(By.XPATH, value="//*[@id='layers']/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/button[2]/div")
+        password.send_keys(os.environ.get("TWITTER_EMAIL"))
+        password.send_keys(Keys.ENTER)
+        sleep(20)
+        tweet_create = self.driver.find_element(By.XPATH, value="//*[@id='react-root']/div/div/div[2]/header/div/div/div/div[1]/div[3]/a/div")
+        tweet_create.send_keys(f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?")
+        sleep(5)
+        tweet_btn = self.driver.find_element(By.XPATH, value="//*[@id='layers']/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div[1]/div/div/div/div[2]/div[2]/div/div/div/button[2]/div")
+        tweet_btn.click()
+        sleep(5)
+        self.driver.quit()
--- Intermediate+/d51_twitter_complaint/main.py
+++ Intermediate+/d51_twitter_complaint/main.py
@@ -1,10 +1,4 @@
-import os
 from ist_bot import InternetSpeedTwitterBot
-
-PROMISED_DOWN = 150
-PROMISED_UP = 10
-TWITTER_EMAIL = os.environ.get("TWITTER_EMAIL")
-TWITTER_PASSWORD = os.environ.get("TWITTER_PASSWORD")
 
 isp_bot = InternetSpeedTwitterBot()
 isp_bot.get_internet_speed()
