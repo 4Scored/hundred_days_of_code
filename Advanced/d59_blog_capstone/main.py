cb8d0197a3840a769c6c7bb6ce0632b0c68f1017
John <johnzionwong33@gmail.com>
2025-06-30 16:34:37 -0700

    d59_blog done

--- Advanced/d59_blog_capstone/main.py
+++ Advanced/d59_blog_capstone/main.py
@@ -1,11 +1,17 @@
+import requests
 from flask import Flask, render_template
 
+NPOINT_BLOG_URL = "https://api.npoint.io/5c42df40015e1d608a1c"
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
+posts = requests.get(NPOINT_BLOG_URL)
+posts.raise_for_status()
+posts_data = posts.json()
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", all_posts=posts_data)
 
 @app.route('/about')
 def about():
@@ -14,6 +20,12 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
+
+@app.route('/post/<int:post_idx>')
+def get_post(post_idx):    
+    for post in posts_data:
+        if post["id"] == post_idx:
+            return render_template("post.html", post=post)
 
 if __name__ == "__main__":
     app.run(debug=True)
