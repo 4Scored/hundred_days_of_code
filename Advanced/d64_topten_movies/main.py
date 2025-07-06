4c5f90601b2c6e61291349a400aeae09cb22c591
John <johnzionwong33@gmail.com>
2025-07-05 23:47:47 -0700

    d64_topten_movies delete implemented

--- Advanced/d64_topten_movies/main.py
+++ Advanced/d64_topten_movies/main.py
@@ -70,6 +70,13 @@ def update():
         return redirect(url_for("home")) 
     return render_template("edit.html", movie=movie_to_update, form=form)
 
+@app.route("/delete")
+def delete():
+    movie_to_delete = db.get_or_404(Movie, request.args.get("id"))
+    db.session.delete(movie_to_delete)
+    db.session.commit()
+    return redirect(url_for("home"))
+
 if __name__ == '__main__':
     app.run(debug=True)
 
