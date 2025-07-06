5cdb95cc43cda60cb60d40237ca96926bb45b483
John <johnzionwong33@gmail.com>
2025-07-05 23:40:49 -0700

    d64_topten_movies view and update implemented

--- Advanced/d64_topten_movies/main.py
+++ Advanced/d64_topten_movies/main.py
@@ -1,9 +1,12 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("APP_SECRET_KEY")
@@ -27,10 +30,52 @@ class Movie(db.Model):
     review: Mapped[str] = mapped_column(String(500), nullable=False)
     img_url: Mapped[str] = mapped_column(String(500), nullable=False)
 
-with app.app_context(): # initialize 
-    db.create_all()
+# with app.app_context(): # initialize 
+#     db.create_all()
 
 # ADD TO TABLE
+# second_movie_to_add = Movie(
+#     title="Avatar The Way of Water",
+#     year=2022,
+#     description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+#     rating=7.3,
+#     ranking=9,
+#     review="I liked the water.",
+#     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+# )
+
+# with app.app_context():
+#     db.session.add(second_movie_to_add)
+#     db.session.commit()
+
+@app.route("/")
+def home():    
+    selected_movies = db.session.execute(db.select(Movie))
+    all_movies = selected_movies.scalars().all()
+    return render_template("index.html", movies=all_movies) #, movies_len=len(all_movies))
+
+class RateMovieForm(FlaskForm):
+    rating = StringField("Your Rating out of 10 e.g. 7.5", validators=[DataRequired()])
+    review = StringField("Your Review", validators=[DataRequired()])
+    submit = SubmitField("Done")
+
+@app.route("/edit", methods=["GET", "POST"])
+def update():
+    form = RateMovieForm()    
+    movie_to_update = db.get_or_404(Movie, request.args.get("id"))
+    if request.method == "POST" and form.validate_on_submit():
+        movie_to_update.rating = float(form.rating.data)
+        movie_to_update.review = form.review.data
+        db.session.commit()
+        return redirect(url_for("home")) 
+    return render_template("edit.html", movie=movie_to_update, form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+''' Archive - 
+
 movie_to_add = Movie(
     title="Phone Booth",
     year=2002,
@@ -41,27 +86,16 @@ movie_to_add = Movie(
     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
 )
 
-with app.app_context():
-    db.session.add(movie_to_add)
-    db.session.commit()
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+# Red underlines? Install the required packages first: 
+# Open the Terminal in PyCharm (bottom left). 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# On Windows type:
+# python -m pip install -r requirements.txt
 
+# On MacOS type:
+# pip3 install -r requirements.txt
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
+# This will install the packages from requirements.txt for this project.
 
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
 '''
