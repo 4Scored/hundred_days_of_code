faf92052d4ef449a759ae1c3f9f1d77792ba32f3
John <johnzionwong33@gmail.com>
2025-07-08 00:06:54 -0700

    d66_restful_api search loc implemented

--- Advanced/d66_restful_api/main.py
+++ Advanced/d66_restful_api/main.py
@@ -27,6 +27,21 @@ class Cafe(db.Model):
     can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
+    def to_dict(self): # helper function; makes it clear as well
+        return {
+            "id": self.id,
+            "name": self.name,
+            "map_url": self.map_url,
+            "img_url": self.img_url,
+            "location": self.location,
+            "seats": self.seats,
+            "has_toilet": self.has_toilet,
+            "has_wifi": self.has_wifi,
+            "has_sockets": self.has_sockets,
+            "can_take_calls": self.can_take_calls,
+            "coffee_price": self.coffee_price
+        }
+
 with app.app_context():
     db.create_all()
 
@@ -34,46 +49,29 @@ with app.app_context():
 def home():
     return render_template("index.html")
 
+# HTTP GET - Read Record
 @app.route("/random", methods=["GET"])
 def get_random_cafe():
     all_cafes = db.session.execute(db.select(Cafe)).scalars().all()
     rand_cafe = random.choice(all_cafes)
-    return jsonify({
-        "id": rand_cafe.id,
-        "name": rand_cafe.name,
-        "map_url": rand_cafe.map_url,
-        "img_url": rand_cafe.img_url,
-        "location": rand_cafe.location,
-        "seats": rand_cafe.seats,
-        "has_toilet": rand_cafe.has_toilet,
-        "has_wifi": rand_cafe.has_wifi,
-        "has_sockets": rand_cafe.has_sockets,
-        "can_take_calls": rand_cafe.can_take_calls,
-        "coffee_price": rand_cafe.coffee_price
-    })
+    return jsonify(rand_cafe.to_dict())
 
 @app.route("/all", methods=["GET"])
 def get_all_cafes():
     cafe_jsons = []
     all_cafes = db.session.execute(db.select(Cafe)).scalars().all()    
     for cafe in all_cafes:
-        cafe_jsons.append({
-            "id": cafe.id,
-            "name": cafe.name,
-            "map_url": cafe.map_url,
-            "img_url": cafe.img_url,
-            "location": cafe.location,
-            "seats": cafe.seats,
-            "has_toilet": cafe.has_toilet,
-            "has_wifi": cafe.has_wifi,
-            "has_sockets": cafe.has_sockets,
-            "can_take_calls": cafe.can_take_calls,
-            "coffee_price": cafe.coffee_price
-        })
+        cafe_jsons.append(cafe.to_dict())
     return jsonify(cafes=cafe_jsons) # jsonify({"cafes": cafe_jsons}) equivalent
 
-
-# HTTP GET - Read Record
+@app.route("/search", methods=["GET"])
+def get_searched_cafe():        
+    searched_cafe_jsons = []
+    search_loc = request.args.get("loc")
+    searched_cafes = db.session.execute(db.select(Cafe).where(Cafe.location == search_loc)).scalars().all()    
+    for cafe in searched_cafes:
+        searched_cafe_jsons.append(cafe.to_dict())
+    return jsonify(cafes=searched_cafe_jsons)
 
 # HTTP POST - Create Record
 
